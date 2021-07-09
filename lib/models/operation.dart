6906ca0b8e6c00644c7a02bc45ee416e8db0c10c
sonylap <alonzoftware@gmail.com>
2021-07-08 22:38:48 -0400

    Generate Operations

---
+++ lib/models/operation.dart
@@ -0,0 +1,11 @@
+class Operation {
+  late int factor1;
+  late int factor2;
+  late int result;
+
+  Operation({
+    required this.factor1,
+    required this.factor2,
+    required this.result,
+  });
+}
--- lib/pages/audio_page.dart
+++ lib/pages/audio_page.dart
@@ -1,4 +1,8 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_multiplication_trainer_app/models/operation.dart';
+import 'package:flutter_multiplication_trainer_app/preferences/shared_preferences.dart';
 import 'package:flutter_multiplication_trainer_app/widgets/bottom_navbar.dart';
 
 class AudioPage extends StatelessWidget {
@@ -12,11 +16,47 @@ class AudioPage extends StatelessWidget {
         child: Column(
           children: [
             Expanded(
-              child: Container(),
+              child: ListOperations(),
             ),
             BottomNavBar(),
           ],
         ),
+      ),
+    );
+  }
+}
+
+class ListOperations extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final prefs = sharedPrefsService;
+    Random rnd;
+    int min = prefs.minLimit;
+    int max = prefs.maxLimit;
+    int factor1 = 0;
+    int factor2 = 0;
+    int result = 0;
+    int count = 25;
+    List<Operation> operations = [];
+    for (int i = 0; i < count; i++) {
+      rnd = new Random();
+
+      factor1 = min + rnd.nextInt(max - min);
+      factor2 = min + rnd.nextInt(max - min);
+      result = factor1 * factor2;
+      print('$i : $factor1 $factor2 $result');
+      Operation opTemp =
+          new Operation(factor1: factor1, factor2: factor2, result: result);
+      operations.add(opTemp);
+    }
+    return Container(
+      child: ListView.builder(
+        itemCount: operations.length,
+        itemBuilder: (BuildContext context, int index) {
+          return ListTile(
+              title: Text(
+                  '${operations[index].factor1} X ${operations[index].factor2} =  ${operations[index].result}'));
+        },
       ),
     );
   }
